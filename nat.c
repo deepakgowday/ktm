f7ebabf50f932e86347ef08ccb4c1dd19603d20b
deepakgowday <43372256+deepakgowday@users.noreply.github.com>
2018-09-18 16:26:58 +0530

    Update nat.c

--- nat.c
+++ nat.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int i,sum=1,n;
+int i,sum=0,n;
 printf("enter the numbers");
 
 scanf("%d",&n);
@@ -10,5 +10,5 @@ for(i=1;i<=n;i++)
     sum=sum+i;
 }
 printf("%d",sum);
-
+return 0;
 }
