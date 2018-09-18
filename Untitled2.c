9d5d8c9a0179aeebb1bc3b8653a3744ada8b1b0b
deepak <gowdadeepak880@gmail.com>
2018-09-18 11:22:37 +0530

    first program

---
+++ Untitled2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main()
+{
+
+    int a=0,b=1,c,i,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    printf(" the series");
+
+
+    for(i=0;i<=n;i++)
+
+    {
+
+     printf("%d",a);
+       c=a+b;
+        a=b;
+        b=c;
+
+    }
+}
