d6ca0a0610fa40ca4b711e8035d7a405786b6be2
deepak <gowdadeepak880@gmail.com>
2018-09-18 12:38:15 +0530

    2nd program

--- Untitled2.c
+++ Untitled2.c
@@ -2,11 +2,31 @@
  int main()
 {
 
-    int a=0,b=1,c,i,n;
+    int a=0,b=1,s,i,n;
     printf("enter the number");
     scanf("%d",&n);
     printf(" the series");
-   for(i=0;i<=n;i++)
+   for(i=1;i<=n;i++)
    {
-       c=a+b;
-        printf("%d",
+     if(i==1)
+     {
+         s=0;
+     }
+     else if(i==2)
+
+     {
+         s=1;
+     }
+     else
+     {
+
+
+       s=a+b;
+
+        a=b;
+        b=s;
+     }
+   }
+  printf("%d",s);
+
+}
